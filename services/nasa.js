cfb1d63813530596bd0adf91f58aa1290775ad28
Philip Winchester <philipwinc@gmail.com>
2016-10-27 16:35:05 -0400

    fixed ES6 class attempt

--- services/nasa.js
+++ services/nasa.js
@@ -1,42 +1,33 @@
 const fetch = require("node-fetch");
 
-class NasaData {
+const APOD_URL = "https://api.nasa.gov/planetary/apod?api_key="
+const EPIC_URL = "https://api.nasa.gov/EPIC/api/v1.0/images.php?api_key="
+const API_KEY  = process.env.NASA_KEY;
 
-  constructor(){
-    this.APOD_URL = "https://api.nasa.gov/planetary/apod?api_key="
-    this.EPIC_URL = "https://api.nasa.gov/EPIC/api/v1.0/images.php?api_key="
-    this.API_KEY = process.env.NASA_KEY;
-    this.health = 5;
-  }
-
-  getApod(req,res, next) {
-    console.log(this.health);
-    fetch(`${this.APOD_URL}${this.API_KEY}`)
-    .then(r => r.json())
-    .then((result) => {
-      res.apod = result;
-      next();
-    })
-    .catch((err) => {
-      res.err = err;
-      next();
-    });
-  };
-
-  getEpic(req,res,next) {
-    fetch(`${this.EPIC_URL}${this.API_KEY}`)
-    .then(r => r.json())
-    .then((result) => {
-      res.epic = result;
-      next();
-    })
-    .catch((err) => {
-      res.err = err;
-      next();
-    });
-  };
+function getApod(req,res, next) {
+  fetch(`${APOD_URL}${API_KEY}`)
+  .then(r => r.json())
+  .then((result) => {
+    res.apod = result;
+    next();
+  })
+  .catch((err) => {
+    res.err = err;
+    next();
+  });
 };
 
-let nasaService = new NasaData();
+function getEpic(req,res,next) {
+  fetch(`${EPIC_URL}${API_KEY}`)
+  .then(r => r.json())
+  .then((result) => {
+    res.epic = result;
+    next();
+  })
+  .catch((err) => {
+    res.err = err;
+    next();
+  });
+};
 
-module.exports = { nasaService }
+module.exports = { getApod, getEpic }
