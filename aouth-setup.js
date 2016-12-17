62461cccd1c9d44e8dfdac6cf3bd36d02f35d5e3
Philip Winchester <philipwinc@gmail.com>
2016-12-16 22:34:37 -0500

    added temp for aoath temp file

---
+++ aouth-setup.js
@@ -0,0 +1,3 @@
+DJwekdli334jdalkjdfjkweY
+
+curl -X "POST" -d "email=philipwinc@gmail.com" -d "password=sunsetPassword" -d "key=wZmZXCyzr2xTZAkxhJ2gaUodi7Uj4xyB" "https://sunburst.sunsetwx.com/v1/register"
