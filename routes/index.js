2817843df0ade01c03ceebf569d874f2c05ef475
Philip Winchester <philipwinc@gmail.com>
2017-03-04 17:44:31 -0500

    updated index route to send html not ejs

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const { randomUnsplashImage } = require("../services/unsplash");
+const index = require('express').Router();
+const path = require('path');
+// const { randomUnsplashImage } = require("../services/unsplash");
 
-const indexRouter = express.Router();
+// const index = express.Router();
 
 // This is the route that serves your '/' homepage
-indexRouter.get('/', randomUnsplashImage, (req, res) => {
-  res.render('index', {
-    bgImage: res.backgroundImage
-  });
+index.get('/', (req, res) => {
+  // res.render('index', {
+  //   bgImage: res.backgroundImage
+  // });
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // This route serves your `/login` form
-indexRouter.get('/login', (req, res) => {
+index.get('/login', (req, res) => {
   res.render('login');
 });
 
 // This route serves your `/signup` form
-indexRouter.get('/signup', (req, res) => {
+index.get('/signup', (req, res) => {
   res.render('signup');
 });
 
-module.exports = indexRouter;
+module.exports = index;
