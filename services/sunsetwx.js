ebcda8480f67b86be7767705308fa73724e303a9
Philip Winchester <philipwinc@gmail.com>
2017-03-09 11:39:15 -0500

    updated gitignore to include some test files

--- services/sunsetwx.js
+++ services/sunsetwx.js
@@ -7,32 +7,6 @@ const API_EMAIL = process.env.SUNSETWX_EMAIL;
 const API_KEY = process.env.SUNSETWX_KEY;
 const API_PASSWORD = process.env.SUNSETWX_PASSWORD;
 
-const sunsetwx = new SunsetWx({
-  email: process.env.SUNSETWX_EMAIL,
-  password: process.env.SUNSETWX_PASSWORD
-})
-
-function sunsewxLogin(req,res,next) {
-  console.log('SunsetWx Login');
-  fetch(`https://sunburst.sunsetwx.com/v1/login`, {
-    header:{
-      "Content-Type": "application/json"
-    },
-    method: 'POST',
-    mode: 'cors',
-    body: JSON.stringify({
-      email: 'philipwinc@gmail.com',
-      password: API_PASSWORD
-    })
-  })
-  .then(r => r.json())
-  .then(auth => {
-    console.log(auth);
-    res.sunsetwx_authorization = auth;
-  })
-  .catch(err => console.log(err))
-}
-
 function getSunsetWXData(req,res,next) {
   const header = {
     "Authorization" : "Bearer " + res.sunsetwx_authorization
